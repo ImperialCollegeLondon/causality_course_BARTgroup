e83e5e6fb5c42d5367f3bf9c00cf28fe45db3646
George Hutchings <george.hutchings@18@imperial.ac.uk>
2024-03-04 21:30:49 +0000

    update

--- project_code/naive_method.R
+++ project_code/naive_method.R
@@ -1,16 +1,23 @@
 set.seed(12345)
 script_directory = dirname(rstudioapi::getSourceEditorContext()$path)
 
+
+fits = list()
+count = 0
 for (val in c('A', 'B', 'C', 'D')){
   for (val2 in c('', '_uneven')){
+    count = count + 1
     val = paste0(val, val2)
     data = read.csv(paste0(script_directory, '/data/data', val, '.csv'))
     data_treated = data[data$A==1,]
     data_untreated = data[data$A==0,]
     #data = rbind(data_treated, data_untreated[floor(nrow(data_untreated)/2),])
     fit = lm(Y ~ . , data = data)
+    fits[[count]] = fit
     print(paste0('data', val))
     print(fit$coefficients["A"])
     print(confint(fit, "A"))
 }
 }
+
+#plot boxplot of FIT with the A coeefficient and a 2 sided ci
--- project_code/propensity_matching.R
+++ project_code/propensity_matching.R
@@ -1,15 +1,28 @@
 library(dplyr)
-library(fastDummies)
 library(rstudioapi)
+library(MatchIt)
 set.seed(12345)
 script_directory = dirname(rstudioapi::getSourceEditorContext()$path)
 
 dataA = read.csv(paste0(script_directory, '/data/dataA.csv'))
 
-dataA_treated = data[data$A==1,]
-dataA_untreated = data[data$A==0,]
+data =dataA
+m.out0 <- matchit(A ~ . , data = data,
+                  method = NULL, distance = "glm")
+summary(m.out0)
 
 
-for (i in seq(10)){
-  
-}
+i=1
+dataA_treated = data[data$A==1,i]
+dataA_untreated = data[data$A==0,i]
+
+xx = c(max(table(dataA_treated)), max(table(dataA_untreated)))/ c(length(dataA_treated), length(dataA_treated))
+# If numeric, create histograms for treated data
+hist(dataA_treated, col = rgb(0, 0, 1, 0.5), main = "Histogram of Treated vs. Untreated Data",
+     xlab = "Values", ylab = "Frequency", xlim = range(c(dataA_treated, dataA_untreated)), ylim=c(0,max(xx)), freq=FALSE)
+
+# Add histogram for untreated data with transparency to distinguish between the two histograms
+hist(dataA_untreated, col = rgb(1, 0, 0, 0.5), add = TRUE, freq=FALSE)
+
+# Add a legend
+legend("topright", legend = c("Treated", "Untreated"), fill = c(rgb(0, 0, 1, 0.5), rgb(1, 0, 0, 0.5)))
