33cbad32941c2cf44fecc7a7168a40f7c416072b
Marcos Tapia Costa <marcostapia4@gmail.com>
2024-03-04 16:51:38 +0000

    DoubleML code

--- configs/double_ml_config.py
+++ configs/double_ml_config.py
@@ -6,4 +6,12 @@ def get_config():
     config.dataA_path = str(Path(__file__).parent.parent) + "/project_code/data/dataA.csv"
     config.dataB_path = str(Path(__file__).parent.parent) + "/project_code/data/dataB.csv"
     config.dataC_path = str(Path(__file__).parent.parent) + "/project_code/data/dataC.csv"
+    config.dataD_path = str(Path(__file__).parent.parent) + "/project_code/data/dataD.csv"
+    config.alpha = 0.05
+    config.n_trees = 200
+    config.n_folds = 5
+    config.n_reps = 1
+    config.true_ATE = -100
+    config.score_func = 'IV-type' # vs  "partialling out"
+    config.image_path = str(Path(__file__).parent.parent) + "/pngs/{}Alpha_{}NTrees_{}NFolds_{}NReps_{}ScoreF.png".format(config.alpha, config.n_trees, config.n_folds, config.n_reps, config.score_func)
     return config
