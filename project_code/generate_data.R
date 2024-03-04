8703bfef27598f1d29d9accbf62fb17602b27e50
George Hutchings <george.hutchings@18@imperial.ac.uk>
2024-03-04 16:36:02 +0000

    add DataD with interaction Y~A + A:x_3 term as discussed with Marcus

--- project_code/generate_data.R
+++ project_code/generate_data.R
@@ -30,7 +30,7 @@ write.csv(data, paste0(script_directory, '/data/dataA.csv'), row.names=FALSE)
 # generate data C
 coeff = c(1, rep(c(0.01,-0.01), 10)[1:6])
 forms <- list(list(),
-              list(A ~ 1 + x_1^2 + x_3^2 + x_4^2 +x_1*x_3 + x_1*x_4 + x_3*x_4 ),
+              list(A ~ 1 + x_1^2 + x_3^2 + x_4^2 +x_1:x_3 + x_1:x_4 + x_3:x_4 ),
               list(Y ~ A),
               list(~ 1))
 fams <- list(integer(0), 5, 1, 1)
@@ -65,14 +65,16 @@ write.csv(data, paste0(script_directory, '/data/dataC.csv') , row.names=FALSE)
 ## generate data D
 forms <- list(list(),
               list(A ~ x_1 + x_3 + x_4),
-              list(Y ~ A + exp(A)),
+              list(Y ~ A + x_1:A),
               list(~ 1))
 fams <- list(integer(0), 5, 1, 1)
 pars <- list(A = list(beta=c(-1.5,0.03,0.02,0.05)),
-             Y = list(beta=c(3200, -100, -100), phi=400^2),
+             Y = list(beta=c(3200, 150, -10), phi=400^2),
              cop = list(beta=-1))
 
-#print(paste0('TDataD True ATE: -100'))
+
+
+print(paste0('TDataD True ATE (approx): ', 150 + round(mean(dat$x_1))*-10))
 data <- rfrugalParam(formulas=forms, family=fams, pars=pars, dat=dat)
 print(summary(data$A))
 write.csv(data, paste0(script_directory, '/data/dataD.csv') , row.names=FALSE)
