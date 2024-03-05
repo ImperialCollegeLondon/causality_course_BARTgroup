1aef45bd3e739f18cf4c3bb1e2c6490e20208fcc
George Hutchings <george.hutchings@18@imperial.ac.uk>
2024-03-05 15:04:33 +0000

    add propensity matching method

--- project_code/naive_method.R
+++ project_code/naive_method.R
@@ -1,23 +1,46 @@
+library(ggplot2)
+
 set.seed(12345)
+# directory of the script
 script_directory = dirname(rstudioapi::getSourceEditorContext()$path)
 
 
-fits = list()
+coefficients = list()
+ci_lower = list()
+ci_upper = list()
+name_list = list()
 count = 0
+# loop through all data files
 for (val in c('A', 'B', 'C', 'D')){
   for (val2 in c('', '_uneven')){
     count = count + 1
     val = paste0(val, val2)
+
+    #read data
     data = read.csv(paste0(script_directory, '/data/data', val, '.csv'))
-    data_treated = data[data$A==1,]
-    data_untreated = data[data$A==0,]
-    #data = rbind(data_treated, data_untreated[floor(nrow(data_untreated)/2),])
+    #fit linear model
     fit = lm(Y ~ . , data = data)
-    fits[[count]] = fit
-    print(paste0('data', val))
-    print(fit$coefficients["A"])
-    print(confint(fit, "A"))
+    #save coefficient value
+    coefficients[[count]] = fit$coefficients["A"]
+    ci_lower[[count]] = confint(fit, "A")[1]
+    ci_upper[[count]] = confint(fit, "A")[2]
+    #save name of data
+    name_list[[count]] = val
 }
 }
 
-#plot boxplot of FIT with the A coeefficient and a 2 sided ci
+#plot coefficient estimates and confidence intervals and true value in dashed line (true value =-100)
+coefficients = unlist(coefficients)
+ci_lower = unlist(ci_lower)
+ci_upper = unlist(ci_upper)
+name_list = unlist(name_list)
+plot_data = data.frame(name_list, coefficients, ci_lower, ci_upper)
+ggplot(plot_data, aes(x=name_list, y=coefficients, ymin=ci_lower, ymax=ci_upper))+
+  geom_point(color= 'blue') +
+  geom_errorbar(width=0.25, color = 'blue') +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(title="ATE estimates and CI (95%) using naive method", x="Data", y="ATE estimate") +
+  geom_hline(yintercept=-100, linetype="dashed", color = "red")
+# save plot
+directory = dirname(script_directory)
+ggsave(paste0(directory, '/pngs/naive_method.png'), dpi = 300)
--- project_code/propensity_matching.R
+++ project_code/propensity_matching.R
@@ -1,28 +1,70 @@
-library(dplyr)
 library(rstudioapi)
 library(MatchIt)
+library(ggplot2)
 set.seed(12345)
+
+# directory of the script
 script_directory = dirname(rstudioapi::getSourceEditorContext()$path)
+directory = dirname(script_directory)
 
-dataA = read.csv(paste0(script_directory, '/data/dataA.csv'))
+coefficients = list()
+ci_lower = list()
+ci_upper = list()
+name_list = list()
+count = 0
+# loop through all data files
+for (val in c('A', 'B', 'C', 'D')){
+  for (val2 in c('', '_uneven')){
+    print(paste0('Processing: data_',val, val2) )
+    
+    count = count + 1
+    val = paste0(val, val2)
+    
+    # read data
+    data = read.csv(paste0(script_directory, '/data/data', val, '.csv'))
+    # fit propensity score model and match data using optimal matching
+    m.out1 <- matchit(A ~ . -Y , data = data, distance = "glm", method= "optimal")
+    
+    #plotting distribution of propensity scores before and after matching
+    try(dev.off())
+    pdf(paste0(directory, '/pngs/distribution_propensity_matching_plot_', val, '.pdf'))
+    plot(m.out1, type = "jitter", interactive = FALSE)
+    dev.off()
+    
+    #matched data
+    data = match.data(m.out1, data=data)
+    data = subset(data, select = -c(distance, weights, subclass))
+    
+    
+    
+    #fit linear model
+    fit = lm(Y ~ . , data = data)
+    #save coefficient value
+    coefficients[[count]] = fit$coefficients["A"]
+    ci_lower[[count]] = confint(fit, "A")[1]
+    ci_upper[[count]] = confint(fit, "A")[2]
+    
+    # NB match it matchit package suggests using avg_comparisons() to estimate 
+    # ATE, however they note in some cases this is superfluous i belive this is 
+    # one of these cases and i have verified that it produces the same results
+    
+    #save name of data
+    name_list[[count]] = val
+}
+}
 
-data =dataA
-m.out0 <- matchit(A ~ . , data = data,
-                  method = NULL, distance = "glm")
-summary(m.out0)
+#plot coefficient estimates and confidence intervals and true value in dashed line (true value =-100)
+coefficients = unlist(coefficients)
+ci_lower = unlist(ci_lower)
+ci_upper = unlist(ci_upper)
+name_list = unlist(name_list)
+plot_data = data.frame(name_list, coefficients, ci_lower, ci_upper)
+ggplot(plot_data, aes(x=name_list, y=coefficients, ymin=ci_lower, ymax=ci_upper))+
+  geom_point(color= 'blue') +
+  geom_errorbar(width=0.25, color = 'blue') +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(title="ATE estimates and CI (95%) using propensity matching", x="Data", y="ATE estimate") +
+  geom_hline(yintercept=-100, linetype="dashed", color = "red")
+# save plot
+ggsave(paste0(directory, '/pngs/propensity_matching_method.png'), dpi = 300)
 
-
-i=1
-dataA_treated = data[data$A==1,i]
-dataA_untreated = data[data$A==0,i]
-
-xx = c(max(table(dataA_treated)), max(table(dataA_untreated)))/ c(length(dataA_treated), length(dataA_treated))
-# If numeric, create histograms for treated data
-hist(dataA_treated, col = rgb(0, 0, 1, 0.5), main = "Histogram of Treated vs. Untreated Data",
-     xlab = "Values", ylab = "Frequency", xlim = range(c(dataA_treated, dataA_untreated)), ylim=c(0,max(xx)), freq=FALSE)
-
-# Add histogram for untreated data with transparency to distinguish between the two histograms
-hist(dataA_untreated, col = rgb(1, 0, 0, 0.5), add = TRUE, freq=FALSE)
-
-# Add a legend
-legend("topright", legend = c("Treated", "Untreated"), fill = c(rgb(0, 0, 1, 0.5), rgb(1, 0, 0, 0.5)))
